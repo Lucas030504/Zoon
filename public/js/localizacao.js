5f370a344171034f3c29af9ef8f9a8240f795ef8
Lucas030504 <lbarroscarregozi@gmail.com>
2023-10-01 15:11:21 -0300

    commit

--- public/js/localizacao.js
+++
@@ -1,7 +0,0 @@
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        var lat = position.coords.latitude
-        var lon = position.coords.longitude
-        document.querySelector('p').innerHTML = "latitude: " + lat + "<br>longitude: " + lon
-    })
-}
